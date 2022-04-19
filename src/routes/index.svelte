05a82566ec06572f0f143c818c23e3b5e11f9731
Fractal-tess <vgfractal@gmail.com>
2022-04-20 02:23:14 +0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,52 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="welcome">
+			<picture>
+				<source srcset="svelte-welcome.webp" type="image/webp" />
+				<img src="svelte-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+	</h1>
+	<h1>There is nothing here for now...</h1>
+	<h2>You might want to go to <span><a href="/videos"> videos</a></span>?</h2>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+	.welcome {
+		position: relative;
+		width: 100%;
+		height: 0;
+		padding: 0 0 calc(100% * 495 / 2048) 0;
+	}
+
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
---
+++ src/routes/videos.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { browser } from '$app/env';
+	import VideoPlayer from 'svelte-video-player';
+
+	const poster = 'http://frontier.jet-black.xyz:3000/malice-logo.jpg';
+	const vod1 = 'http://frontier.jet-black.xyz:3000/vod1.mp4';
+	const vod2 = 'http://frontier.jet-black.xyz:3000/vod2.mp4';
+	const vod3 = 'http://frontier.jet-black.xyz:3000/vod3.mp4';
+</script>
+
+{#if browser}
+	<div>
+		<h2>2022-04-17</h2>
+		<VideoPlayer {poster} source={vod1} />
+	</div>
+
+	<div>
+		<h2>2022-04-16</h2>
+		<VideoPlayer {poster} source={vod2} />
+	</div>
+	<div>
+		<h2>2022-04-09</h2>
+		<VideoPlayer {poster} source={vod3} />
+	</div>
+{/if}
