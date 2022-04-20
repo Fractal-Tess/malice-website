ec7bc0b5411c797520a18a959ec16899078a09a8
Fractal-tess <vgfractal@gmail.com>
2022-04-20 04:44:35 +0300

    fixed file path

--- src/routes/videos.svelte
+++ src/routes/videos.svelte
@@ -2,10 +2,10 @@
 	import { browser } from '$app/env';
 	import VideoPlayer from 'svelte-video-player';
 
-	const poster = 'https://frontier.jet-black.xyz:3000/malice-logo.jpg';
-	const vod1 = 'https://frontier.jet-black.xyz:3000/vod1.mp4';
-	const vod2 = 'https://frontier.jet-black.xyz:3000/vod2.mp4';
-	const vod3 = 'https://frontier.jet-black.xyz:3000/vod3.mp4';
+	const poster = 'https://frontier.jet-black.xyz/malice-logo.jpg';
+	const vod1 = 'https://frontier.jet-black.xyz/vod1.mp4';
+	const vod2 = 'https://frontier.jet-black.xyz/vod2.mp4';
+	const vod3 = 'https://frontier.jet-black.xyz/vod3.mp4';
 </script>
 
 {#if browser}
