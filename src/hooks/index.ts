d357bf16829b6c9e1fde7a25d5e85dae4e6dd5aa
Fractal-tess <vgfractal@gmail.com>
2022-05-02 16:46:19 +0300

    bugfix

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -3,9 +3,16 @@ import { parse } from 'cookie';
 import { isTheme } from '../types';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const cookies = parse(event.request.headers.get('cookie'));
-	if (cookies.theme && isTheme(cookies.theme)) {
-		event.locals.theme = cookies.theme || null;
+	const cookie = event.request.headers.get('cookie');
+	if (cookie) {
+		// Get cookies
+		const cookies = parse(cookie);
+
+		// Handle theme
+		if (cookies.theme && isTheme(cookies.theme)) {
+			event.locals.theme = cookies.theme || null;
+		}
+		// Handle other cookies
 	}
 	if (event.routeId === 'videos/[raid]_[folder]_[clip]')
 		return await resolve(event, { ssr: false });
